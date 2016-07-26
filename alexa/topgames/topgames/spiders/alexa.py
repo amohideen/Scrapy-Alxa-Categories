528909b690323d64308f0d20fc94e6310aa6f645
amohideen <althaff.mohideen@gmail.com>
2016-07-26 15:10:47 +0100

    Alexa

---
+++ alexa/topgames/topgames/spiders/alexa.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from topgames.items import TopgamesItem
+
+
+class AlexaSpider(CrawlSpider):
+    name = 'alexa'
+    allowed_domains = ['alexa.com']
+    start_urls = ['http://www.alexa.com/topsites/category/Top/Games']
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths='//*[@class="alexa-pagination"]'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+	selector = response.xpath('//*[@id="alx-content"]/div/div/section[2]/span/span/section/ul/li/div[2]/p/a/@href')
+	for i in selector.extract():
+		yield {"url": i.replace('/siteinfo/', 'www.')}
